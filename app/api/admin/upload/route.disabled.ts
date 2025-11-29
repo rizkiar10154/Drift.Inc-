1c8ca599d1a9ac6f574dccef8dfbbb371d2c3f64
rizkiar10154 <rizkyarif196@gmail.com>
2025-11-30 00:27:50 +0700

    change upload route to route.disabled.ts

--- app/api/admin/upload/route.disabled.ts
+++ app/api/admin/upload/route.disabled.ts
@@ -1,4 +1,4 @@
-/*
+
 import { NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
@@ -27,4 +27,3 @@ export async function POST(req: Request) {
     return NextResponse.json({ success: false, message: "Upload failed" }, { status: 500 });
   }
 }
-*/
